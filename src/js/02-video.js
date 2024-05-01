902534283d6b5e35d6dd6772174f4dd9bf5647f3
Sergey Shishkin <Shishkin_SU@ukr.net>
2024-05-01 12:04:44 +0300

    finish task

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,34 @@
+import Player from "@vimeo/player";
+import throttle from "lodash.throttle";
 
+const iframe = document.querySelector("#vimeo-player");
+
+const STORAGE_KEY = "videoplayer - current - time";
+const player = new Player(iframe);
+
+const onPlay = function ({ seconds }) {
+  localStorage.setItem(STORAGE_KEY, seconds);
+};
+player.on("timeupdate", throttle(onPlay, 1000));
+
+let storageTime;
+
+const currentTime = localStorage.getItem(STORAGE_KEY);
+if (currentTime) {
+  storageTime = currentTime;
+} else {
+  storageTime = 0;
+}
+
+player
+  .setCurrentTime(storageTime)
+  .then(function (seconds) {})
+  .catch(function (error) {
+    switch (error.name) {
+      case "RangeError":
+        break;
+
+      default:
+        break;
+    }
+  });
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,48 @@
+import throttle from "lodash.throttle";
 
+const STORAGE_KEY = "feedback-form-state";
+
+const form = document.querySelector(".feedback-form");
+
+form.addEventListener("submit", handlerSubmit);
+form.addEventListener("input", throttle(handlerInput, 500));
+formFilling();
+
+function handlerInput(event) {
+  if (event.currentTarget) {
+    const inputData = formValue(event);
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(inputData));
+  }
+}
+
+function handlerSubmit(event) {
+  event.preventDefault();
+  const { email, message } = formValue(event);
+  if (email === "" || message === "") {
+    return alert("Всі поля повинні бути заповнені");
+  } else {
+    const formData = {
+      email: email,
+      message: message,
+    };
+    console.log("formData :>> ", formData);
+  }
+  form.reset();
+  localStorage.clear();
+}
+
+function formFilling() {
+  if (localStorage.getItem(STORAGE_KEY)) {
+    const { email, message } = JSON.parse(localStorage.getItem(STORAGE_KEY));
+    form.email.value = email;
+    form.message.value = message;
+  }
+}
+
+function formValue(data) {
+  const inputData = {
+    email: data.currentTarget.email.value,
+    message: data.currentTarget.message.value,
+  };
+  return inputData;
+}
